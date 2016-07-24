b5206c14fcd5332b57f3697370e5288919ef046c
Edward Chen <edwardjchen@gmail.com>
2016-07-24 17:59:12 +0000

    node server post and get

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -22,6 +22,13 @@ export class MessageInputComponent {
     
     onSubmit(form: any) {
         const message: Message = new Message(form.content, null, 'Dummy');
-        this._messageService.addMessage(message);
+        this._messageService.addMessage(message)
+            .subscribe(
+                data => {
+                    console.log(data)
+                    this._messageService.messages.push(data); 
+                },
+                error => console.error(error)
+            );
     }
 }
--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -1,14 +1,38 @@
 import {Message} from "./message"; 
+import {Http, Headers} from "angular2/http"; 
+import {Injectable} from "angular2/core"; 
+import 'rxjs/Rx'; 
+import {Observable} from "rxjs/Observable"; 
+@Injectable()
 export class MessageService {
     messages: Message[] = [];
     
+    constructor (private _http: Http) {}
+    
     addMessage(message: Message) {
-        this.messages.push(message); 
-        console.log(this.messages); 
+        const body = JSON.stringify(message); 
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this._http.post('https://webdevmean-coded5282.c9users.io/message', body, {headers: headers})
+            .map(response => {
+                const data = response.json().obj;
+                let message = new Message(data.content, data._id, 'Dummy', null);
+                return message; 
+            })
+            .catch(error => Observable.throw(error.json())); 
     }
     
     getMessages() {
-        return this.messages; 
+        return this._http.get('https://webdevmean-coded5282.c9users.io/message')
+            .map(response => {
+                const data = response.json().obj;
+                let objs: any[] = [];
+                for (let i = 0; i < data.length; i++) {
+                    let message = new Message(data[i].content, data[i]._id, 'Dummy', null);
+                    objs.push(message);
+                };
+                return objs; 
+            })
+            .catch(error => Observable.throw(error.json()));
     }
     
     editMessage(message: Message) {
